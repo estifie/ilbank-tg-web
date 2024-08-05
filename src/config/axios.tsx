644ee8e918626bed41a25f1cc811df6f9090745f
LntAxe <ertusari0@gmail.com>
2024-08-05 14:17:24 +0300

    feat: Add axios config

---
+++ src/config/axios.tsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { config } from "dotenv";
+config();
+
+const api = axios.create({
+	baseURL: process.env.REACT_APP_API_URL,
+	headers: {
+		"Content-Type": "application/json",
+	},
+});
+
+api.interceptors.request.use(
+	(config) => {
+		const token = localStorage.getItem("token");
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	},
+);
+
+export default api;
