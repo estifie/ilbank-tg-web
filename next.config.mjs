9611c8bb65cd454e3ee19e9ef083c2ccac69b6ff
LntAxe <ertusari0@gmail.com>
2024-08-05 14:16:00 +0300

    refactor: Update next.config for redirection

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: "/",
+				destination: "/programlar",
+				permanent: true,
+			},
+		];
+	},
+};
 
 export default nextConfig;
