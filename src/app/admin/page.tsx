2ddfda033c27120db7a6d0d895c91923122c8580
LntAxe <ertusari0@gmail.com>
2024-08-05 14:18:44 +0300

    refactor: Update admin page and add login

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -40,7 +40,10 @@ import {
 	DropdownMenuTrigger,
 } from "@components/ui/dropdown-menu";
 import { Input } from "@components/ui/input";
+import { useAuth } from "@context/AuthContext";
 import { Data, ProgramType } from "@interfaces/data";
+import Link from "next/link";
+import Login from "./components/login";
 
 const data: Data[] = [
 	{
@@ -201,12 +204,11 @@ export const columns: ColumnDef<Data>[] = [
 					</DropdownMenuTrigger>
 					<DropdownMenuContent align="end">
 						<DropdownMenuLabel>İşlemler</DropdownMenuLabel>
+
+						<DropdownMenuSeparator />
 						<DropdownMenuItem onClick={() => navigator.clipboard.writeText(payment.code.toString())}>
 							Programı Sil
 						</DropdownMenuItem>
-						<DropdownMenuSeparator />
-						<DropdownMenuItem>View customer</DropdownMenuItem>
-						<DropdownMenuItem>View payment details</DropdownMenuItem>
 					</DropdownMenuContent>
 				</DropdownMenu>
 			);
@@ -214,14 +216,14 @@ export const columns: ColumnDef<Data>[] = [
 	},
 ];
 
-export default function DataTableDemo() {
+export default function Admin() {
 	const [sorting, setSorting] = React.useState<SortingState>([]);
 	const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
 	const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({});
 	const [rowSelection, setRowSelection] = React.useState({});
 	const [pagination, setPagination] = React.useState({ pageIndex: 0, pageSize: 5 });
+	const { jwtToken } = useAuth();
 
-	// add one page is 5 rows
 	const table = useReactTable({
 		data,
 		columns,
@@ -245,142 +247,159 @@ export default function DataTableDemo() {
 
 	return (
 		<main className="min-h-screen flex flex-col items-center p-4 md:p-24 flex-1 justify-center">
-			<div className="w-full mb-10">
-				<h1 className="font-bold text-2xl">Programları Düzenle</h1>
-				<h1
-					style={{
-						color: "#64748B",
-					}}
-				>
-					Tablo üzerinden programları inceleyebilir, dilediğiniz programı düzenleyebilirsiniz. Sağ üst köşede
-					bulunan
-					<br />
-					menüden sütunları gösterebilir veya gizleyebilirsiniz.
-				</h1>
-			</div>
-			<div className="w-full">
-				<div className="flex items-center justify-between py-4">
-					<Input
-						placeholder="Program adı ara..."
-						value={(table.getColumn("programName")?.getFilterValue() as string) ?? ""}
-						onChange={(event) => table.getColumn("programName")?.setFilterValue(event.target.value)}
-						className="max-w-sm"
-					/>
-					<div>
-						<DropdownMenu>
-							<DropdownMenuTrigger asChild>
-								<Button variant="outline" className="ml-auto">
-									Sütunlar <ChevronDown className="ml-2 h-4 w-4" />
+			{jwtToken ? (
+				<>
+					<div className="w-full mb-10">
+						<h1 className="font-bold text-2xl">Programları Düzenle</h1>
+						<h1
+							style={{
+								color: "#64748B",
+							}}
+						>
+							Tablo üzerinden programları inceleyebilir, dilediğiniz programı düzenleyebilirsiniz. Sağ üst
+							köşede bulunan
+							<br />
+							menüden sütunları gösterebilir veya gizleyebilirsiniz.
+						</h1>
+					</div>
+					<div className="w-full">
+						<div className="flex items-center justify-between py-4">
+							<div className="flex flex-row">
+								<Input
+									placeholder="Program adı ara..."
+									value={(table.getColumn("programName")?.getFilterValue() as string) ?? ""}
+									onChange={(event) =>
+										table.getColumn("programName")?.setFilterValue(event.target.value)
+									}
+									className="max-w-sm mr-6"
+								/>
+								<Button>
+									<Link href={"/program-ekle"}>Program Ekle</Link>
 								</Button>
-							</DropdownMenuTrigger>
-							<DropdownMenuContent align="end">
-								{table
-									.getAllColumns()
-									.filter((column) => column.getCanHide())
-									.map((column) => {
-										return (
-											<DropdownMenuCheckboxItem
-												key={column.id}
-												className="capitalize"
-												checked={column.getIsVisible()}
-												onCheckedChange={(value) => column.toggleVisibility(!!value)}
-											>
-												{ColumnName[column.id as keyof typeof ColumnName]}
-											</DropdownMenuCheckboxItem>
-										);
-									})}
-							</DropdownMenuContent>
-						</DropdownMenu>
-					</div>
-				</div>
-				<div className="rounded-md border">
-					<Table>
-						<TableHeader>
-							{table.getHeaderGroups().map((headerGroup) => (
-								<TableRow key={headerGroup.id}>
-									{headerGroup.headers.map((header) => {
-										return (
-											<TableHead key={header.id}>
-												{header.isPlaceholder
-													? null
-													: flexRender(header.column.columnDef.header, header.getContext())}
-											</TableHead>
-										);
-									})}
-								</TableRow>
-							))}
-						</TableHeader>
-						<TableBody>
-							{table.getRowModel().rows?.length ? (
-								table.getRowModel().rows.map((row) => (
-									<TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
-										{row.getVisibleCells().map((cell) => (
-											<TableCell key={cell.id}>
-												{flexRender(cell.column.columnDef.cell, cell.getContext())}
+							</div>
+
+							<div>
+								<DropdownMenu>
+									<DropdownMenuTrigger asChild>
+										<Button variant="outline" className="ml-auto">
+											Sütunlar <ChevronDown className="ml-2 h-4 w-4" />
+										</Button>
+									</DropdownMenuTrigger>
+									<DropdownMenuContent align="end">
+										{table
+											.getAllColumns()
+											.filter((column) => column.getCanHide())
+											.map((column) => {
+												return (
+													<DropdownMenuCheckboxItem
+														key={column.id}
+														className="capitalize"
+														checked={column.getIsVisible()}
+														onCheckedChange={(value) => column.toggleVisibility(!!value)}
+													>
+														{ColumnName[column.id as keyof typeof ColumnName]}
+													</DropdownMenuCheckboxItem>
+												);
+											})}
+									</DropdownMenuContent>
+								</DropdownMenu>
+							</div>
+						</div>
+						<div className="rounded-md border">
+							<Table>
+								<TableHeader>
+									{table.getHeaderGroups().map((headerGroup) => (
+										<TableRow key={headerGroup.id}>
+											{headerGroup.headers.map((header) => {
+												return (
+													<TableHead key={header.id}>
+														{header.isPlaceholder
+															? null
+															: flexRender(
+																	header.column.columnDef.header,
+																	header.getContext(),
+															  )}
+													</TableHead>
+												);
+											})}
+										</TableRow>
+									))}
+								</TableHeader>
+								<TableBody>
+									{table.getRowModel().rows?.length ? (
+										table.getRowModel().rows.map((row) => (
+											<TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+												{row.getVisibleCells().map((cell) => (
+													<TableCell key={cell.id}>
+														{flexRender(cell.column.columnDef.cell, cell.getContext())}
+													</TableCell>
+												))}
+											</TableRow>
+										))
+									) : (
+										<TableRow>
+											<TableCell colSpan={columns.length} className="h-24 text-center">
+												Program bulunamadı.
 											</TableCell>
-										))}
-									</TableRow>
-								))
-							) : (
-								<TableRow>
-									<TableCell colSpan={columns.length} className="h-24 text-center">
-										Program bulunamadı.
-									</TableCell>
-								</TableRow>
-							)}
-						</TableBody>
-					</Table>
-				</div>
-				<div className="flex items-center justify-end space-x-2 py-4">
-					<div className="flex-1 text-sm text-muted-foreground">
-						{table.getFilteredRowModel().rows.length} programdan{" "}
-						{table.getFilteredSelectedRowModel().rows.length} tanesi seçildi.
-						<AlertDialog>
-							<AlertDialogTrigger asChild>
+										</TableRow>
+									)}
+								</TableBody>
+							</Table>
+						</div>
+						<div className="flex items-center justify-end space-x-2 py-4">
+							<div className="flex-1 text-sm text-muted-foreground">
+								{table.getFilteredRowModel().rows.length} programdan{" "}
+								{table.getFilteredSelectedRowModel().rows.length} tanesi seçildi.
 								<div className="mt-8">
-									<Button
-										variant="destructive"
-										disabled={table.getFilteredSelectedRowModel().rows.length === 0}
-									>
-										Seçilenleri Sil
-									</Button>
+									<AlertDialog>
+										<AlertDialogTrigger asChild>
+											<Button
+												variant="destructive"
+												disabled={table.getFilteredSelectedRowModel().rows.length === 0}
+											>
+												Seçilenleri Sil
+											</Button>
+										</AlertDialogTrigger>
+										<AlertDialogContent>
+											<AlertDialogHeader>
+												<AlertDialogTitle>Emin misin?</AlertDialogTitle>
+												<AlertDialogDescription>
+													Bu işlem geri alınamaz. Seçilen programların bütün detaylarını ve
+													verilerini kalıcı olarak silmek istediğinize emin misiniz?
+												</AlertDialogDescription>
+											</AlertDialogHeader>
+											<AlertDialogFooter>
+												<AlertDialogCancel>İptal</AlertDialogCancel>
+												<AlertDialogAction>Evet, Eminim</AlertDialogAction>
+											</AlertDialogFooter>
+										</AlertDialogContent>
+									</AlertDialog>
 								</div>
-							</AlertDialogTrigger>
-							<AlertDialogContent>
-								<AlertDialogHeader>
-									<AlertDialogTitle>Emin misin?</AlertDialogTitle>
-									<AlertDialogDescription>
-										Bu işlem geri alınamaz. Seçilen programların bütün detaylarını ve verilerini
-										kalıcı olarak silmek istediğinize emin misiniz?
-									</AlertDialogDescription>
-								</AlertDialogHeader>
-								<AlertDialogFooter>
-									<AlertDialogCancel>İptal</AlertDialogCancel>
-									<AlertDialogAction>Evet, Eminim</AlertDialogAction>
-								</AlertDialogFooter>
-							</AlertDialogContent>
-						</AlertDialog>
+							</div>
+							<div className="space-x-2">
+								<Button
+									variant="outline"
+									size="sm"
+									onClick={() => table.previousPage()}
+									disabled={!table.getCanPreviousPage()}
+								>
+									Önceki
+								</Button>
+								<Button
+									variant="outline"
+									size="sm"
+									onClick={() => table.nextPage()}
+									disabled={!table.getCanNextPage()}
+								>
+									Sonraki
+								</Button>
+							</div>
+						</div>
 					</div>
-					<div className="space-x-2">
-						<Button
-							variant="outline"
-							size="sm"
-							onClick={() => table.previousPage()}
-							disabled={!table.getCanPreviousPage()}
-						>
-							Previous
-						</Button>
-						<Button
-							variant="outline"
-							size="sm"
-							onClick={() => table.nextPage()}
-							disabled={!table.getCanNextPage()}
-						>
-							Next
-						</Button>
-					</div>
-				</div>
-			</div>
+				</>
+			) : (
+				<Login />
+			)}
 		</main>
 	);
 }
