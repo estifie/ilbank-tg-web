7f7c3af5ab1b4c6a0c1c8671e03df871056db6dc
LntAxe <ertusari0@gmail.com>
2024-08-06 11:40:26 +0300

    refactor: Update login page in admin route

--- src/app/admin/components/login.tsx
+++ src/app/admin/components/login.tsx
@@ -6,7 +6,10 @@ import { Label } from "@components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@components/ui/radio-group";
 import { useAuth } from "@context/AuthContext";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
 const formSchema = z.object({
@@ -29,7 +32,7 @@ const formSchema = z.object({
 });
 
 export default function Login() {
-	const { login } = useAuth();
+	const { login, loading } = useAuth();
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
@@ -40,42 +43,68 @@ export default function Login() {
 	});
 
 	function onSubmit(values: z.infer<typeof formSchema>) {
+		toast("Giriş yapılıyor", {
+			description: "Lütfen biraz bekleyin...",
+		});
 		login(values);
 	}
 
 	return (
-		<div className="flex items-center">
-			<Form {...form}>
-				<form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
-					<FormField
-						control={form.control}
-						name="username"
-						render={({ field }) => (
-							<FormItem className="mb-8">
-								<FormLabel className="text-gray-700">Kullanıcı Adı</FormLabel>
-								<FormControl>
-									<Input placeholder="Kullanıcı adınızı girin..." {...field} className="w-96" />
-								</FormControl>
-								<FormMessage />
-							</FormItem>
+		<main className="min-h-screen flex flex-col items-center p-4 md:p-24 flex-1 justify-center">
+			<div className="flex items-center flex-col justify-center mb-[6vw]">
+				<Image
+					src="/logo.png"
+					alt="Logo"
+					width={283}
+					height={324}
+					className="mb-8"
+					style={{
+						width: "10vw",
+						height: "10vw",
+						userSelect: "none",
+						pointerEvents: "none",
+					}}
+					draggable={false}
+				/>
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+						<FormField
+							control={form.control}
+							name="username"
+							render={({ field }) => (
+								<FormItem className="mb-8">
+									<FormLabel className="text-gray-700">Kullanıcı Adı</FormLabel>
+									<FormControl>
+										<Input placeholder="Kullanıcı adınızı girin..." {...field} className="w-96" />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="password"
+							render={({ field }) => (
+								<FormItem className="mb-8">
+									<FormLabel className="text-gray-700">Şifre</FormLabel>
+									<FormControl>
+										<Input placeholder="Şifrenizi girin..." {...field} className="w-96" />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						{loading ? (
+							<Button disabled>
+								<Loader2 className="mr-2 h-4 w-4 animate-spin" />
+								Lütfen bekleyin...
+							</Button>
+						) : (
+							<Button type="submit">Giriş Yap</Button>
 						)}
-					/>
-					<FormField
-						control={form.control}
-						name="password"
-						render={({ field }) => (
-							<FormItem className="mb-8">
-								<FormLabel className="text-gray-700">Şifre</FormLabel>
-								<FormControl>
-									<Input placeholder="Şifrenizi girin..." {...field} className="w-96" />
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<Button type="submit">Giriş Yap</Button>
-				</form>
-			</Form>
-		</div>
+					</form>
+				</Form>
+			</div>
+		</main>
 	);
 }
