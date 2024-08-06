ca44ae63a8466d4e1c74259f9bc98dced130f874
LntAxe <ertusari0@gmail.com>
2024-08-06 11:40:12 +0300

    feat: Create 'kullanici-ekle' route

---
+++ src/app/admin/kullanici-ekle/page.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { Button } from "@components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@components/ui/form";
+import { Input } from "@components/ui/input";
+import { Label } from "@components/ui/label";
+import withAuth from "@hoc/withAuth";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+	username: z.string().min(1, {
+		message: "Kullanıcı adı boş bırakılamaz.",
+	}),
+	sifre: z.string().min(8, {
+		message: "Şifre en az 8 karakter olmalıdır.",
+	}),
+});
+
+function ProgramEkle() {
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			username: "",
+			sifre: "",
+		},
+	});
+
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		console.log(values);
+	}
+
+	return (
+		<main className="min-h-screen flex flex-col items-center p-4 md:p-24 flex-1">
+			<div className="w-full mb-10">
+				<h1 className="font-bold text-2xl">Yeni Kullanıcı Ekle</h1>
+				<h1
+					style={{
+						color: "#64748B",
+					}}
+				>
+					Yeni bir kullanıcı ekleyin. Eklediğiniz kullanıcılar, admin paneline erişebilir ve
+					<br />
+					programlar üzerinde değişiklik yapabilir.
+				</h1>
+			</div>
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-8 w-full"
+				>
+					<FormField
+						control={form.control}
+						name="username"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className="text-gray-700">Kullanıcı Adı</FormLabel>
+								<FormControl>
+									<Input placeholder="Örn. mehmet" {...field} className="w-96" />
+								</FormControl>
+								<FormDescription>Giriş yaparken girilecek kullanıcı adı</FormDescription>
+
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="sifre"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className="text-gray-700">Şifre</FormLabel>
+								<FormControl>
+									<Input placeholder="Örn. ********" {...field} className="w-96" />
+								</FormControl>
+								<FormDescription>
+									Giriş yaparken kullanılacak şifre. Güvenlik nedeniyle şifrenizin en az 8 karakter
+									olması önerilir.
+								</FormDescription>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<Button type="submit" className="w-64">
+						Kullanıcı Ekle
+					</Button>
+					<Label className="text-gray-700">Giriş bilgilerini kullanıcı haricinde kimseyle paylaşmayın.</Label>
+				</form>
+			</Form>
+		</main>
+	);
+}
+
+export default withAuth(ProgramEkle);
