8899ea84b9a04be00cbdb146c115d3bed4c4e18c
LntAxe <ertusari0@gmail.com>
2024-08-14 10:35:44 +0300

    refactor: Update withAuth HOC

--- src/hoc/withAuth.tsx
+++ src/hoc/withAuth.tsx
@@ -1,12 +1,21 @@
 import Login from "@/app/admin/components/login";
 import { useAuth } from "@context/AuthContext";
-import React, { ComponentType } from "react";
+import React, { ComponentType, useEffect, useState } from "react";
 
 const withAuth = <P extends object>(WrappedComponent: ComponentType<P>) => {
 	const ComponentWithAuth: React.FC<P> = (props) => {
 		const { jwtToken, loading } = useAuth();
+		const [isClient, setIsClient] = useState<boolean>(false);
 
-		if (!jwtToken) {
+		useEffect(() => {
+			setIsClient(true);
+		}, []);
+
+		if (!isClient) {
+			return null;
+		}
+
+		if (!jwtToken && !loading) {
 			return <Login />;
 		}
 
