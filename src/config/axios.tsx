c460e08ebe9ba2ade498d64b5c525f141e58c7c4
LntAxe <ertusari0@gmail.com>
2024-08-14 10:37:01 +0300

    fix: Add CORS support

--- src/config/axios.tsx
+++ src/config/axios.tsx
@@ -3,8 +3,8 @@ import { config } from "dotenv";
 config();
 
 const api = axios.create({
-	baseURL: process.env.REACT_APP_API_URL,
 	headers: {
+		"Access-Control-Allow-Origin": "*",
 		"Content-Type": "application/json",
 	},
 });
