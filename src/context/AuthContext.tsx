78efc6fcf2e4d4faba167e807d6b8419c092882f
LntAxe <ertusari0@gmail.com>
2024-08-14 10:36:47 +0300

    refactor: Update AuthContext for ability to add new users

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,12 @@
 import api from "@config/axios";
+import { config } from "dotenv";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
+import { toast } from "sonner";
+config();
+
+const BASE_URL = "http://localhost:8080/tg/api";
 
 interface UserLoginType {
 	username: string;
@@ -11,41 +18,66 @@ interface AuthContextType {
 	login: (credentials: UserLoginType) => Promise<void>;
 	logout: () => void;
 	loading: boolean;
+	addUser: (credentials: UserLoginType) => Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-	const [jwtToken, setJwtToken] = useState<string | null>(localStorage.getItem("jwtToken"));
+	const [jwtToken, setJwtToken] = useState<string | null>(Cookies.get("jwtToken") || null);
 	const [loading, setLoading] = useState<boolean>(false);
+	const router = useRouter();
 
 	useEffect(() => {
 		if (jwtToken) {
-			localStorage.setItem("jwtToken", jwtToken);
+			api.defaults.headers.Authorization = `Bearer ${jwtToken}`;
 		} else {
-			localStorage.removeItem("jwtToken");
+			api.defaults.headers.Authorization = null;
 		}
 	}, [jwtToken]);
 
 	const login = async (credentials: UserLoginType) => {
 		setLoading(true);
 		try {
-			const response = await api.post("/tg/api/auth/login", credentials);
-			setJwtToken(response.data.token);
-			api.defaults.headers.Authorization = `Bearer ${response.data.token}`;
+			const response = await api.post(BASE_URL + "/login", credentials);
+			const token = response.data.token;
+			Cookies.set("jwtToken", token);
+			setJwtToken(token || null);
 		} catch (error) {
+			toast.error("Giriş yapılırken bir hata oluştu.", {
+				duration: 800,
+			});
 			console.error(error);
 		} finally {
 			setLoading(false);
 		}
 	};
 
-	const logout = () => {
-		setJwtToken(null);
-		api.defaults.headers.Authorization = null;
+	const addUser = async (credentials: UserLoginType) => {
+		try {
+			const response = await api.post(BASE_URL + "/admin/register", credentials);
+			const token = response.data.token;
+			Cookies.set("jwtToken", token);
+			setJwtToken(token || null);
+			router.push("/admin");
+		} catch (error) {
+			toast.error("Kullanıcı eklenirken bir hata oluştu.", {
+				duration: 5000,
+			});
+			console.error(error);
+		}
 	};
 
-	return <AuthContext.Provider value={{ jwtToken, login, logout, loading }}>{children}</AuthContext.Provider>;
+	const logout = () => {
+		Cookies.remove("jwtToken");
+		setJwtToken(null);
+		api.defaults.headers.Authorization = null;
+		toast.success("Başarıyla çıkış yapıldı.");
+	};
+
+	return (
+		<AuthContext.Provider value={{ jwtToken, login, logout, loading, addUser }}>{children}</AuthContext.Provider>
+	);
 };
 
 const useAuth = (): AuthContextType => {
