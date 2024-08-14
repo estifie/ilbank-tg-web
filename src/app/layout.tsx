425a251328142359d25e98285d1653c97dcb09db
LntAxe <ertusari0@gmail.com>
2024-08-14 10:40:21 +0300

    feat: Add new providers for backend communication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { Toaster } from "@/components/ui/sonner";
 import { AuthProvider } from "@context/AuthContext";
+import { DirectorateProvider } from "@context/DirectorateContext";
+import { ProgramProvider } from "@context/ProgramContext";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -13,10 +15,14 @@ export default function RootLayout({
 }>) {
 	return (
 		<AuthProvider>
-			<html lang="en">
-				<body className={inter.className}>{children}</body>
-				<Toaster />
-			</html>
+			<ProgramProvider>
+				<DirectorateProvider>
+					<html lang="en">
+						<body className={inter.className}>{children}</body>
+						<Toaster />
+					</html>
+				</DirectorateProvider>
+			</ProgramProvider>
 		</AuthProvider>
 	);
 }
