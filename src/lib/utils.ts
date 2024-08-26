ca6630fe4849b5a421fb0940cf4270b3735481b2
LntAxe <ertusari0@gmail.com>
2024-08-26 16:08:24 +0300

    style: Change displayUserTypes function name

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
-export function displayUserTypes(users: string[]) {
+export function displayArray(users: string[]) {
 	return users.join(", ") || "Kullanıcı Yok";
 }
 
